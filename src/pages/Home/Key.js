985fb500ecc9fed2cf95d0a956cf2e6f8b355f52
Danny Roberts <dannyroberts.b@gmail.com>
2021-08-24 10:19:14 +0100

    got the service key from xcode and trying to render it to the UI

--- src/pages/Home/Key.js
+++ src/pages/Home/Key.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+// import 'info.plist';
+// import 'strings.xml';
+
+// import {SERVICE_KEY} from '/ios/rnFinal/info';
 
 const Key = () => {
+  // let KEY = Bundle.main.object(forInfoDictionaryKey: “Service_Key”)
+  // print(serverUrl)
   return (
     <SafeAreaView>
       <View>
-        <Text>I am not the key</Text>
+        {/* <Text>{SERVICE_KEY}</Text> */}
+        <Text>I am going to be a key</Text>
       </View>
     </SafeAreaView>
   );
